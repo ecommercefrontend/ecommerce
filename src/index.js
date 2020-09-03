fd84086f9a8a8b325dd949994e7287689f642639
hsoubecommerce <hsoubecommerce@gmail.com>
2020-09-03 10:36:55 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-// import '@laylazi/bootstrap-rtl-scss/dist/css/bootstrap-rtl.min.css';
-// import "@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss";
-
 import './scss/style.scss';
 import './css/style.css';
 import 'webpack-jquery-ui/css';
 import 'webpack-jquery-ui';
 import '@fortawesome/fontawesome-free/js/all.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import 'jquery/dist/jquery.min'; 
 import 'popper.js/dist/popper.min';
+import 'jquery/dist/jquery.min'; 
 import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min.js';
 
 
